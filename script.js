4388dee5605e9e7d97f2bae3e76cb23a41bc2209
mingyyu <65578403+mingyyu@users.noreply.github.com>
2023-12-15 15:09:15 +0800

    fixed values

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-import { secretValue } from "./secret";
-
 function checkInput() {
   var inputValue = document.getElementById("inputField").value;
 
-  if (inputValue === secretValue) {
+  if (inputValue === "5kKXX") {
     document.getElementById("result").innerHTML =
-      "<h4>6435129085648913644873748</h4>";
+      "<h4>EGva76XFr5fjCt4qKSebPpHd</h4>";
   } else {
     document.getElementById("result").innerHTML = "<p>WRONG PASSWORD</p>";
   }
